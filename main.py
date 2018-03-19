cdc308fe259e57fd441edb3171a55aa8277267de
Vedant <vshetty.vs@gmail.com>
2018-03-19 11:08:00 +0530

    Simplified code

--- main.py
+++ main.py
@@ -1,24 +1,13 @@
 import speech_recognition as sr
-
-mic_name = "Bus 001 Device 002: ID 0cf3:e301 Qualcomm Atheros Communications "
-samprate = 48000
-cs = 2048
-
 r = sr.Recognizer()
-
-mic_list = sr.Microphone.list_microphone_names()
-print(mic_list)
-for i, microphone_name in enumerate(mic_list):
-	if microphone_name == mic_name:
-		device_id = 1
-
-with sr.Microphone(device_index = device_id, sample_rate = samprate, chunck_size=cs) as  source:
+with sr.Microphone() as source:
 	r.adjust_for_ambient_noise(source)
-	print('Speak Biatch')
+	print("Say something!")
 	audio = r.listen(source)
-	
-	try:
-		t = r.recognice_google(audio)
-		print(t)
-	except:
-		print('Error')
+try:
+    print("You said: " + r.recognize_google(audio))
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
